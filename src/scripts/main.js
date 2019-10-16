c8a48c85ad741a9a3e7cca3ce35e5f71efdbf816
Corri Golden <corri.golden@gmail.com>
2019-10-16 09:09:52 -0500

    finish p mac

---
+++ src/scripts/main.js
@@ -0,0 +1,36 @@
+console.log("Lets get it started in here")
+
+const beatles = {
+    albums: ["Abbey Road", "Sgt Peppers Lonely Heart's Club Band", "Revolver", "Magical Mystery Tour", "Something New"],
+    history: {
+        formed: 1960,
+        disbanded: 1970
+    },
+    members: [
+        {
+            name: "George Harrison",
+            birth: 1943,
+            death: 2001
+        },
+        {
+            name: "Paul McCartney",
+            birth: 1942,
+            death: null
+        },
+        {
+            name: "John Lennon",
+            birth: 1940,
+            death: 1980
+        },
+        {
+            name: "Ringo Starr",
+            birth: 1940,
+            death: null
+        }
+    ]
+}
+
+
+let chris = beatles.members[1].name 
+console.log(`${chris}` + " was in the Beatles")
+
